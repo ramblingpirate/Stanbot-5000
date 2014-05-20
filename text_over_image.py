f33f3d4c47ce72d3920dada0378d97c9f276d845
Cpt. Edward Teach (The dreaded Black Pirate) <ramblingpirate@gmail.com>
2014-05-20 00:09:44 -0500

    PEP8 compliant and fixed bottom text bug

--- text_over_image.py
+++ text_over_image.py
@@ -22,24 +22,27 @@ def text_to_image(imagefile, tquote, bquote):
     draw = ImageDraw.Draw(image)
 
     tfont = find_size(tquote, image.size[0])
-    draw.text((image.size[0]*.10-2,0), tquote, fill='black', 
+    draw.text((image.size[0]*.10-2, 0), tquote, fill='black',
               font=tfont, anchor=image.size[0]/2)
-    draw.text((image.size[0]*.10+2,0), tquote, fill='black', 
+    draw.text((image.size[0]*.10+2, 0), tquote, fill='black',
               font=tfont, anchor=image.size[0]/2)
-    draw.text((image.size[0]*.10,2), tquote, fill='black', 
+    draw.text((image.size[0]*.10, 2), tquote, fill='black',
               font=tfont, anchor=image.size[0]/2)
-    draw.text((image.size[0]*.10,-2), tquote, fill='black',
+    draw.text((image.size[0]*.10, -2), tquote, fill='black',
               font=tfont, anchor=image.size[0]/2)
-    draw.text((image.size[0]*.10,0), tquote, fill='white', 
+    draw.text((image.size[0]*.10, 0), tquote, fill='white',
               font=tfont, anchor=image.size[0]/2)
 
-    bfont = find_size(bquote, image.size[0])
-    draw.text((image.size[0]*.10+2,image.size[1]*.90-2),
-              bquote, fill='black', font=bfont, anchor=image.size[0]/2)
-    draw.text((image.size[0]*.10-2, image.size[1]*.90+2),
-              bquote, fill='black', font=bfont, anchor=image.size[0]/2)
-    draw.text((image.size[0]*.10,image.size[1]*.90),
-              bquote, fill='white', font=bfont, anchor=image.size[0]/2)
+    draw.text((image.size[0]*.10+2, image.size[1]*.90),
+              bquote, fill='black', font=tfont, anchor=image.size[0]/2)
+    draw.text((image.size[0]*.10-2, image.size[1]*.90),
+              bquote, fill='black', font=tfont, anchor=image.size[0]/2)
+    draw.text((image.size[0]*.10, image.size[1]*.90-2),
+              bquote, fill='black', font=tfont, anchor=image.size[0]/2)
+    draw.text((image.size[0]*.10, image.size[1]*.90+2),
+              bquote, fill='black', font=tfont, anchor=image.size[0]/2)
+    draw.text((image.size[0]*.10, image.size[1]*.90),
+              bquote, fill='white', font=tfont, anchor=image.size[0]/2)
 
     image.save('result.png', 'PNG')
 
